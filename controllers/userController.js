10872b518033fed462c194dccd45dd7dfc18f51f
Josal <joyalmeida321@gmail.com>
2021-11-13 00:49:16 +0530

    dont touch

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,20 +16,40 @@ exports.registerUser = (req, res) => {
   const newUser = new User(req.body);
   newUser.save((err, data) => {
     if (err) throw err;
-    //env-> {data._id}
     res.json(data);
   });
 };
 
 //for login
-exports.findUser = (req, res) => {
-  User.find({ _id: ObjectId(req.params.id) }, function (err, data) {
+exports.loginUser = (req, res) => {
+  User.findOne({ email: req.body.email }, function (err, data) {
     if (err) throw err;
-    //get password {data.password}
-    res.send(data);
+    if (data) {
+      if (data.password === req.body.password) {
+        process.env["userID"] = data._id;
+        res.send(data);
+      } else {
+        res.send("Invalid Password");
+      }
+    } else {
+      res.send("Invalid Email");
+    }
   });
 };
 
+// find user
+exports.findUser = (req, res) => {
+  if (process.env.userID == undefined) {
+    message = "session expired. Log in again";
+    res.render("user_login", { message });
+  }
+  //   console.log("obj id in env ", process.env.userID);
+  User.findOne({ _id: ObjectId(process.env.userID) }, function (err, data) {
+    if (err) throw err;
+    // console.log(data);
+    res.send(data);
+  });
+};
 //updating user data
 //send only the data to be updated in key-value
 exports.updateUser = (req, res) => {
@@ -52,11 +72,12 @@ exports.deleteUser = (req, res) => {
     res.json(data);
   });
 };
-
-// module.exports = (app) => {
-//   app.get("/users", getAllUsers);
-//   app.post("/", registerUser);
-//   app.get("/:id", findUser);
-//   app.put("/:id", updateUser);
-//   app.delete("/:id", deleteUser);
-// };
+/*
+module.exports = (app) => {
+  app.get("/users", getAllUsers);
+  app.post("/", registerUser);
+  app.get("/:id", findUser);
+  app.put("/:id", updateUser);
+  app.delete("/:id", deleteUser);
+};
+*/
--- routes/pages.js
+++ routes/pages.js
@@ -1,31 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
+const userController = require("../controllers/userController");
+
 //route for going to homepage of the website
-router.get('/', (req, res) => {
-    res.render('mainhome');
+router.get("/", (req, res) => {
+  //   res.render("mainhome");
+  res.render("user_dashboard");
 });
 
-
 //route to go to authority login page
-router.get('/login_authority',(req,res)=>{
-    res.render('authority_login');
-})
+router.get("/login_authority", (req, res) => {
+  res.render("authority_login");
+});
 
 //route to go to user register page
-router.get('/user_register',(req,res)=>{
-    res.render('user_register');
-})
+router.get("/user_register", (req, res) => {
+  res.render("user_register");
+});
 
 //route to go to authority register page
-router.get('/authority_register',(req,res)=>{
-    res.render('authority_register');
-})
+router.get("/authority_register", (req, res) => {
+  res.render("authority_register");
+});
 
-//route to go to user login page 
-router.get('/login_user',(req,res)=>{
-    res.render('user_login');
-})
+//route to go to user login page
+router.get("/login_user", (req, res) => {
+  res.render("user_login");
+});
 
-
+router.post("/auth/user_register", userController.registerUser);
+router.post("/auth/user/login", userController.loginUser);
+router.get("/auth/user/dashboard", userController.findUser);
 module.exports = router;
