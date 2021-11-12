bbd88309a43abafa63e4190c4ffa7ad6c8334549
Josal <joyalmeida321@gmail.com>
2021-11-12 16:21:38 +0530

    aa

--- models/db.js
+++ models/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 //Connect to the database
@@ -9,15 +8,3 @@ mongoose.connect(
     else console.log("DB Connection Succeeded");
   }
 );
-=======
-const mongoose = require("mongoose");
-
-//Connect to the database
-mongoose.connect(
-  "mongodb+srv://octa-hack:octa-hack@octa-hacks-4.fkdso.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  (err) => {
-    if (err) console.log("DB Connection Error ", err);
-    else console.log("DB Connection Succeeded");
-  }
-);
->>>>>>> 3b0d4817aff8bf634af9a31cc4c779e8538181aa
