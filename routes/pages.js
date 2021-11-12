c945a9b25f4d15cd009fde9d2bd002d10364995b
“Sahil <chorghesahil16@gmail.com>
2021-11-13 01:41:23 +0530

    Sahil

--- routes/pages.js
+++ routes/pages.js
@@ -4,6 +4,17 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('mainhome');
 });
-
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+router.get('/contact', (req, res) => {
+    res.render('contact');
+});
+router.get('/authority', (req, res) => {
+    res.render('authority');
+});
+router.get('/user', (req, res) => {
+    res.render('user');
+});
 
 module.exports = router;
