199e823db351e3ad3c51460f8f57ff0a0d279c4f
jeetmistry <jeetmistry26@gmail.com>
2021-11-12 20:37:34 +0530

    login register ui created

--- routes/pages.js
+++ routes/pages.js
@@ -5,5 +5,23 @@ router.get('/', (req, res) => {
     res.render('mainhome');
 });
 
+router.get('/login_authority',(req,res)=>{
+    res.render('authority_login');
+})
+
+router.get('/user_register',(req,res)=>{
+    res.render('user_register');
+})
+
+
+router.get('/authority_register',(req,res)=>{
+    res.render('authority_register');
+})
+
+
+router.get('/login_user',(req,res)=>{
+    res.render('user_login');
+})
+
 
 module.exports = router;
